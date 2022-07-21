a4c888a2bef74536412cc3496207ca353c9eb8ae
Ravi <ravi@gmail.com>
2022-07-21 12:06:38 +0530

    ec2 code

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -14,4 +14,17 @@ output "x" {
   value = "value of a is ${var.a}"
 }
 
+output "vars" {
+  value = "values, ${var.a}, ${var.b}, ${var.c}, ${var.d} "
+}
+
 // if a variable uses with combination of other strings or in quotes ${} is used
+
+variable "b" {}
+// this variable is declared in  .tfvars or .auto.tfvars file
+
+variable "c" {}
+// this declared from Cli of terraform , command is : terraform apply -auto-approve -var c=400
+
+variable "d" {}
+//this declared from shell, command is : export TF_VAR_d=500
