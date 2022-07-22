80c053e2c7ecd6a7c2f0ce6e4be7854969d1e9a7
Ravi <ravi@gmail.com>
2022-07-22 12:15:02 +0530

    ec2 code

---
+++ 04-loops/approach03/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  count         = length(var.instance_type)
+  ami           = "ami-07fb268aa87edd4d3"
+  instance_type = var.instance_type[count.index]
+
+  tags = {
+    Name = "terraform-${count.index+1}"
+  }
+}
+
+variable "instance_type" {
+  default = [ "t3.micro", "t3.small"]
+}
