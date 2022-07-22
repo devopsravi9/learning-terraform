cb67bdfccadb4305f88173ccc33414111cf5139d
Ravi <ravi@gmail.com>
2022-07-22 15:26:43 +0530

    ec2 code

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -2,10 +2,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-07fb268aa87edd4d3"
   instance_type = var.instance_type == "null" ? "t3.micro" : var.instance_type
-//  count       = 0 (we can write some condition whether this to be created or not )
-  tags = {
-    Name = "terraform-${count.index+1}"
-  }
+  //  count       = 0 (we can write some condition whether this to be created or not )
 }
 
 variable "instance_type" {
