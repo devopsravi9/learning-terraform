71880bce8de08ac49dd1fb2b1cbcbe9c145e7beb
Ravi <ravi@gmail.com>
2022-07-22 12:02:35 +0530

    ec2 code

--- 04-loops/approach02/module/main.tf
+++ 04-loops/approach02/module/main.tf
@@ -1,10 +1,9 @@
 resource "aws_instance" "web" {
   ami           = "ami-07fb268aa87edd4d3"
   instance_type = "t3.micro"
-}
-
-tags = {
-  Name = "var.name_tag"
+  tags          = {
+    Name = "var.name_tag"
+  }
 }
 
 variable "name_tag" {}
