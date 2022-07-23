6550b54d6f285f9bb1abf6d8dd0fe3d745cf1772
Ravi <ravi@gmail.com>
2022-07-23 10:50:13 +0530

    ec2 code

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,7 +1,8 @@
 
 resource "aws_instance" "web" {
-  ami           = "ami-07fb268aa87edd4d3"
-  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+  ami           = data.aws_ami.example.image_id
+  instance_type = local.instance_type
+//  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
   //  count       = 0 (we can write some condition whether this to be created or not )
   tags = {
     Name = "terraform"
@@ -12,4 +13,14 @@ variable "instance_type" {
   default = null
 }
 
+locals {
+  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+}
+
 //condition ? true_val : false_val
+
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "base-with-ansible"
+  owners           = ["self"]
